867908bf87ea225e919a25af4e706801f0fbad48
Younus <younusshamim.bd@gmail.com>
2022-03-02 20:11:12 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+//------- mixitup
+$(document).ready(function () {
+    var mixer = mixitup('.portfolioProjects');
+    var mixer = mixitup(containerEl);
+
+    var mixer = mixitup(containerEl, {
+        selectors: {
+            target: '.blog-item'
+        },
+        animation: {
+            duration: 300
+        }
+    });
+});
+
